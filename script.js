4dccc6039d2101618cd4bee7f3f25dfd21adc1fd
Paul Keldsen <Pkeld148@gmail.com>
2020-12-12 15:22:28 -0500

    added initial html structure, added initial js variables

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("HELLO WORLD");
+
+var quizQuestions = {
+    
+}
+
+var questionOne = "Commonly used data types DO NO include:";
+var questionTwo = "The condition in an if / else statement is enclosed within _____";
+var questionThree = "Arrays in JavaScript can be used to store _____";
+var questionFour = "String values must be enclosed within _____ when being assigned to variables.";
+var questionFive = "A very useful tool used during development and debugging for printing content to the debugger is:"
+
+
+
+var answersQuestionOne = ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"];
+var answersQuestionTwo = ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"];
+var answersQuestionThree = ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"];
+var answersQuestionFour = ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"];
+var answersQuestionFive = ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console.log"];
+
+
+
+
